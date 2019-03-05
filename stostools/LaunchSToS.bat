c8f0678153e4298392df90fcd7c0fa2f380dcd01
Deepak Shankargouda <deshank@microsoft.com>
2019-03-05 10:45:40 -0800

    Revert "Revert "Removing trigger comment parameter""

--- stostools/LaunchSToS.bat
+++ stostools/LaunchSToS.bat
@@ -4,9 +4,8 @@ set StartupProj=%1
 set PrNumber=%2
 set LastGithubCommitId=%3
 set GitHubUrl=%4
-set TriggerComment=%5
-set DebugMode=%6
-set LogVerbosity=%7
+set DebugMode=%5
+set LogVerbosity=%6
 
 if "%VSWHERE%"=="" set "VSWHERE=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
 
@@ -20,11 +19,10 @@ echo StartupProj: %StartupProj%
 echo Pr#: %PrNumber%
 echo Last Commit Id: %LastGithubCommitId%
 echo GitHub Url: %GitHubUrl%
-echo Trigger Comment: %TriggerComment%
 echo DebugMode: %DebugMode%
 echo Log Verbosity: %LogVerbosity%
 
-dotnet msbuild %startupProj% /t:StartNetSdkCodeGeneration /v:%LogVerbosity% /p:githubprnumber=%PrNumber% /p:githubcommitid=%LastGithubCommitId% /p:giturl=%GitHubUrl%  /p:DebugMode=%DebugMode% /p:triggercomment=%TriggerComment%
+dotnet msbuild %startupProj% /t:StartNetSdkCodeGeneration /v:%LogVerbosity% /p:githubprnumber=%PrNumber% /p:githubcommitid=%LastGithubCommitId% /p:giturl=%GitHubUrl%  /p:DebugMode=%DebugMode%
 
 REM dotnet msbuild stos.proj /t:StartNetSdkCodeGeneration /v:N /p:githubprnumber=63 /p:githubcommitid=b5ee45302838515adc14857ed45c44ed99a1e517 /p:giturl=https://github.com/Azure/AzSwaggerSpecsTestClone  /p:DebugMode=false /p:triggercomment=startbuild
 REM LaunchSToS.bat stos.proj 63 b5ee45302838515adc14857ed45c44ed99a1e517 https://github.com/Azure/AzSwaggerSpecsTestClone startbuild false D
